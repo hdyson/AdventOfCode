3929fb975178725bdd038658f4abae27e15444de
Harold Dyson <hdyson@gmail.com>
2020-02-12 21:23:21 +0000

    Added tests

---
+++ Tests/Tests.swift
@@ -0,0 +1,59 @@
+//
+//  TestDayOne.swift
+//  TestDayOne
+//
+//  Created by Harold Dyson on 12/02/2020.
+//  Copyright © 2020 Harold Dyson. All rights reserved.
+//
+
+import XCTest
+
+@testable import AdventOfCode
+
+
+class TestDayOne: XCTestCase {
+
+    var rocket = Rocket(masses: "")
+    
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testPart1Example1() {
+        let expected = 2
+        let actual = rocket.calculate_module_fuel(mass: 12)
+        XCTAssertTrue(actual == expected)
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPart1Example2() {
+        let expected = 2
+        let actual = rocket.calculate_module_fuel(mass: 14)
+        XCTAssertTrue(actual == expected)
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPart1Example3() {
+        let expected = 654
+        let actual = rocket.calculate_module_fuel(mass: 1969)
+        XCTAssertTrue(actual == expected)
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPart1Example4() {
+        let expected = 33583
+        let actual = rocket.calculate_module_fuel(mass: 100756)
+        XCTAssertTrue(actual == expected)
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+}
+
