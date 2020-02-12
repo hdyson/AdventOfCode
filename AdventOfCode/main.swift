f5baa90ea817366840e750d3a3103ab800efa6f6
Harold Dyson <hdyson@gmail.com>
2020-02-12 21:06:45 +0000

    Initial Commit

---
+++ AdventOfCode/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  AdventOfCode
+//
+//  Created by Harold Dyson on 12/02/2020.
+//  Copyright © 2020 Harold Dyson. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
