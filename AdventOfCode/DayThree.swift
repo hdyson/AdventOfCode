8016318a1483236a367a9864e81e6c68b06f41a5
Harold Dyson <hdyson@gmail.com>
2020-02-21 22:52:17 +0000

    Solution for day 3 part 2, but it's too inefficient  

--- AdventOfCode/DayThree.swift
+++ AdventOfCode/DayThree.swift
@@ -16,6 +16,16 @@ enum InstructionError: Error {
 struct Point : Hashable {
     var x=0
     var y=0
+    var index = 0
+    
+    static func == (lhs: Point, rhs: Point) -> Bool {
+        return lhs.x == rhs.x && lhs.y == rhs.y
+    }
+    
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(x)
+        hasher.combine(y)
+    }
 }
 
 
@@ -24,7 +34,7 @@ class DayThree{
     var current_point : Point
     
     init(){
-        self.current_point = Point(x:0, y:0)
+        self.current_point = Point()
     }
     
     func parse (input:String) throws {
@@ -33,9 +43,10 @@ class DayThree{
                 print("Too many input lines")
                 exit(EXIT_FAILURE)
             }
-            current_point = Point(x: 0, y: 0)
+            current_point = Point()
             // split line on , to get each instruction
             let instructions = input_line.trimmingCharacters(in: .whitespacesAndNewlines).components(separatedBy: ",")
+            var step_count = 1
             for var instruction in instructions{
                 if instruction == "" {
                     continue
@@ -46,17 +57,18 @@ class DayThree{
                 for _ in 0 ... magnitude - 1 {
                     switch direction {
                         case "U":
-                            current_point = Point(x: current_point.x, y: current_point.y + 1)
+                            current_point = Point(x: current_point.x, y: current_point.y + 1, index: step_count)
                         case "D":
-                            current_point = Point(x: current_point.x, y: current_point.y - 1)
+                            current_point = Point(x: current_point.x, y: current_point.y - 1, index: step_count)
                         case "R":
-                            current_point = Point(x: current_point.x + 1, y: current_point.y)
+                            current_point = Point(x: current_point.x + 1, y: current_point.y, index: step_count)
                         case "L":
-                            current_point = Point(x: current_point.x - 1, y: current_point.y)
+                            current_point = Point(x: current_point.x - 1, y: current_point.y, index: step_count)
                         default:
                             throw InstructionError.invalidDirection(direction: direction)
                     }
                     line[index].insert(current_point)
+                    step_count += 1
                 }
             }
         }
@@ -66,17 +78,40 @@ class DayThree{
         return abs(input.y) + abs(input.x)
     }
     
-    func solve(script:String) -> Int {
+    func minimum_distance(script:String) -> Int {
         try! parse(input: script)
         let crossings = line[0].intersection(line[1])
         let distances = crossings.map( {manhattan_distance(input: $0)} )
         return distances.min()!
     }
     
+    func step_distance(input: Point) -> Int {
+        return input.index
+    }
+    
+    func get_crossings_with_summed_indices() -> [Point] {
+        var result = [Point]()
+        for point1 in line[0]{
+            for point2 in line[1]{
+                if point1 == point2{
+                    result.append(Point(x: point1.x, y:point1.y, index: point1.index + point2.index))
+                }
+            }
+        }
+        return result
+    }
+    
+    func minimum_steps(script:String) -> Int {
+        try! parse(input: script)
+        let crossings = get_crossings_with_summed_indices()
+        let distances = crossings.map( {step_distance(input: $0)} )
+        return distances.min()!
+    }
+    
 }
 
-func daythree(contents:String) -> Int {
+func daythree(contents:String) -> String {
     let solver = DayThree()
-    return solver.solve(script: contents)
+    return "part 1: " + String(solver.minimum_distance(script: contents)) + " part 2: " + String(solver.minimum_steps(script: contents))
 }
 
