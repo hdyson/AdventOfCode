2f21e039afc9a3cf6f2bcc0b71a5e8018b8a0d35
Harold Dyson <hdyson@gmail.com>
2020-04-13 21:13:01 +0100

    Mahoosive simplifications 😀

--- DaySix/DaySix.swift
+++ DaySix/DaySix.swift
@@ -10,7 +10,7 @@ import Foundation
 
 class DaySixParser {
     var input = ""
-    var astronomicalObjects = Set<AstronomicalObject>()
+    var astronomicalObjects = [String: String]()
 
     func parse(inputString: String) {
         input = inputString
@@ -18,51 +18,22 @@ class DaySixParser {
         let orbitSeparator = ")"
         for line in input.components(separatedBy: lineSeparator) {
             let bodies = line.components(separatedBy: orbitSeparator)
-            let planetName = bodies[0]
-            let planet = AstronomicalObject(planet: nil, satellite: planetName)
+            let planet = bodies[0]
             let satellite = bodies[1]
-            astronomicalObjects.insert(AstronomicalObject(planet: planet, satellite: satellite))
-            if !astronomicalObjects.contains(planet) {
-                astronomicalObjects.insert(AstronomicalObject(planet: nil, satellite: planetName))
-            }
+            astronomicalObjects[satellite] = planet
         }
     }
 
     func countOrbits() -> Int {
-        return 0
-    }
-}
-
-class AstronomicalObject: Hashable {
-    let name: String
-    let orbits: AstronomicalObject?
-    var totalOrbits: Int?
-
-    init(planet: AstronomicalObject?, satellite: String) {
-        name = satellite
-        orbits = planet
-        totalOrbits = nil
-    }
-
-    func countOrbits() -> Int {
-        if totalOrbits == nil {
-            if orbits == nil {
-                totalOrbits = 0
-            } else {
-                totalOrbits = orbits!.countOrbits() + 1
+        var total = 0
+        for (satellite, _) in astronomicalObjects {
+            var nextPlanet = astronomicalObjects[satellite]
+            while nextPlanet != nil {
+                nextPlanet = astronomicalObjects[nextPlanet!]
+                total+=1
             }
         }
-        return totalOrbits!
-    }
-
-    static func == (lhs: AstronomicalObject, rhs: AstronomicalObject) -> Bool {
-        return lhs.name == rhs.name && lhs.orbits == rhs.orbits
-    }
-
-    func hash(into hasher: inout Hasher) {
-        // Need manual hasher for a class (can be auto for struct, but can't use a struct recursively)
-        hasher.combine(name)
-        hasher.combine(orbits)
+        return total
     }
 }
 
--- DaySix/Tests/DaySixTests.swift
+++ DaySix/Tests/DaySixTests.swift
@@ -23,19 +23,6 @@ class DaySixParserTests: XCTestCase {
         XCTAssertEqual(actual, expected)
     }
 
-    func testCreatesAstronomicalObjects() {
-        let testString = "foo)bar"
-
-        let orbited = AstronomicalObject(planet: nil, satellite: "foo")
-        let satellite = AstronomicalObject(planet: orbited, satellite: "bar")
-        let expected = Set([orbited, satellite])
-
-        daysix.parse(inputString: testString)
-        let actual = daysix.astronomicalObjects
-
-        XCTAssertEqual(actual, expected)
-    }
-
     func testCountOrbits() {
         let testString = """
 COM)B
@@ -59,47 +46,4 @@ K)L
         XCTAssertEqual(actual, expected)
     }
 
-}
-
-class AstronomicalObjectTests: XCTestCase {
-
-    let astronomicalObject = AstronomicalObject(
-        planet: AstronomicalObject(planet: nil, satellite: "foo"),
-        satellite: "bar"
-    )
-
-    func testInitSetsName() {
-        let expected = "bar"
-
-        let actual = astronomicalObject.name
-
-        XCTAssertEqual(actual, expected)
-    }
-
-    func testInitSetsOrbits() {
-        let expected = AstronomicalObject(planet: nil, satellite: "foo")
-
-        let actual = astronomicalObject.orbits
-
-        XCTAssertEqual(actual, expected)
-    }
-
-    func testCountSingleOrbit() {
-        let expected = 1
-
-        let actual = astronomicalObject.countOrbits()
-
-        XCTAssertEqual(actual, expected)
-    }
-
-    func testCountMultipleOrbits() {
-        let expected = 3
-
-        let secondObject = AstronomicalObject(planet: astronomicalObject, satellite: "baz")
-        let thirdObject = AstronomicalObject(planet: secondObject, satellite: "end")
-        let actual = thirdObject.countOrbits()
-
-        XCTAssertEqual(actual, expected)
-
-    }
 }
