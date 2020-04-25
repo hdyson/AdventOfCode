41c232cf5dee36a3bb25d6f163156389fdf55697
Harold Dyson <hdyson@gmail.com>
2020-04-25 19:52:58 +0100

    Array permutations

--- DaySeven/DaySeven.swift
+++ DaySeven/DaySeven.swift
@@ -8,12 +8,67 @@
 
 import Foundation
 
+class Solver {
+    var input: Int
+    var amplifiers: [DaySevenParser]
+    var maximumPhase: [Int]
+    var potentialPhases: [[Int]]
+
+    init () {
+        input = 0
+        amplifiers = Array(repeating: DaySevenParser(), count: 5)
+        maximumPhase = [0, 1, 2, 3, 4]
+        potentialPhases = []
+    }
+
+    func solve(script: String) throws {
+        var output = input
+        var phases = [0, 1, 2, 3, 4]
+        for amplifier in amplifiers {
+            amplifier.input = output  // input for next amplifier is output from previous
+            amplifier.phase = phases.removeFirst()
+            _ = try amplifier.parse(script: script)
+            output = amplifier.output!
+        }
+        return
+    }
+
+    func getResult() -> Int {
+        return amplifiers[amplifiers.count - 1].output!
+    }
+    // Following adapted from:
+    // https://stackoverflow.com/questions/34968470/calculate-all-permutations-of-a-string-in-swift#34969212
+    // Keeping similar names, so let's disable the swiftlint name check:
+    // swiftlint:disable identifier_name
+
+    func permute(_ n: Int, _ a: inout [Int]) {
+        if n == 1 {potentialPhases.append(a); return}
+        for i in 0..<n-1 {
+            permute(n-1, &a)
+            a.swapAt(n-1, (n%2 == 1) ? 0 : i)
+        }
+        permute(n-1, &a)
+    }
+}
+
 class DaySevenParser: DayFiveParser {
 
+    var phase: Int?
+    var phaseUsed = false
+
+    override func readInput() {
+        if phaseUsed {
+            elements[elements[instructionPointer + 1]] = phase!
+            instructionPointer += 2
+            phaseUsed = true
+        } else {
+            super.readInput()
+        }
+    }
 }
 
 func dayseven(contents: String) throws -> String {
-    let parser = DaySevenParser()
-    _ = try parser.parse(script: contents)
-    return "Part 1: \(parser.getResult())"
+    let solver = Solver()
+    _ = try solver.solve(script: contents)
+    return "Part 1: \(solver.getResult())"
 }
--- DaySeven/Tests/DaySevenTests.swift
+++ DaySeven/Tests/DaySevenTests.swift
@@ -6,68 +6,92 @@
 //  Copyright © 2020 Harold Dyson. All rights reserved.
 //
 
+// swiftlint:disable force_try
+
 import XCTest
 
 class DaySevenTests: XCTestCase {
 
-    var dayseven = DaySevenParser()
+    var dayseven = Solver()
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func testPhaseExample1() {
+        let testString = "3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0"
+
+        let expected = [4, 3, 2, 1, 0]
+
+        try! dayseven.solve(script: testString)
+        let actual = dayseven.maximumPhase
+
+        XCTAssertEqual(actual, expected)
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    func testPhaseExample2() {
+        let testString = "3,23,3,24,1002,24,10,24,1002,23,-1,23,101,5,23,23,1,24,23,23,4,23,99,0,0"
+
+        let expected = [0, 1, 2, 3, 4]
+
+        try! dayseven.solve(script: testString)
+        let actual = dayseven.maximumPhase
+
+        XCTAssertEqual(actual, expected)
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testPhaseExample3() {
+        let testString = """
+        3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,33,1002,33,7,33,1,33,31,31,1,32,31,31,4,31,99,0,0,0
+        """
+
+        let expected = [1, 0, 4, 3, 2]
+
+        try! dayseven.solve(script: testString)
+        let actual = dayseven.maximumPhase
+
+        XCTAssertEqual(actual, expected)
     }
 
-    func testExample1() {
+    func testMaxSignalExample1() {
         let testString = "3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0"
 
         let expected = 43210
 
-        dayseven.input = 0
-        _ = try! dayseven.parse(script: testString)
-        let actual = dayseven.getResult()[0]
+        try! dayseven.solve(script: testString)
+        let actual = dayseven.getResult()
 
         XCTAssertEqual(actual, expected)
     }
 
-    func testExample2() {
+    func testMaxSignalExample2() {
         let testString = "3,23,3,24,1002,24,10,24,1002,23,-1,23,101,5,23,23,1,24,23,23,4,23,99,0,0"
 
         let expected = 54321
 
-        dayseven.input = 0
-        _ = try! dayseven.parse(script: testString)
-        let actual = dayseven.getResult()[0]
+        try! dayseven.solve(script: testString)
+        let actual = dayseven.getResult()
 
         XCTAssertEqual(actual, expected)
     }
 
-    func testExample3() {
+    func testMaxSignalExample3() {
         let testString = """
         3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,33,1002,33,7,33,1,33,31,31,1,32,31,31,4,31,99,0,0,0
         """
 
         let expected = 65210
 
-        dayseven.input = 0
-        _ = try! dayseven.parse(script: testString)
-        let actual = dayseven.getResult()[0]
+        try! dayseven.solve(script: testString)
+        let actual = dayseven.getResult()
 
         XCTAssertEqual(actual, expected)
     }
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func testPermutations() {
+        let expected = [[0, 1, 2], [1, 0, 2], [2, 0, 1], [0, 2, 1], [1, 2, 0], [2, 1, 0]]
+
+        var testArray = [0, 1, 2]
+        dayseven.permute(testArray.count, &testArray)
+        let actual = dayseven.potentialPhases
+
+        XCTAssertEqual(actual, expected)
     }
 
 }
