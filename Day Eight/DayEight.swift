73d81dada62ba52601be0c29a52de0a58dd446fd
Harold Dyson <hdyson@gmail.com>
2020-05-10 19:35:50 +0100

    Day 8 part 2

--- Day Eight/DayEight.swift
+++ Day Eight/DayEight.swift
@@ -8,23 +8,28 @@
 
 import Foundation
 
+enum LayerError: Error {
+    case invalidDimensions
+}
+
 class DayEightSolver {
 
-    var layers = [[Int]]()
+    var layers = [Layer]()
     var layerWithFewestZeroes = Int.max
 
-    init(data: String, width: Int, height: Int) {
+    init(data: String, width: Int, height: Int) throws {
         let data = data.compactMap({ Int(String($0)) })
         let width = width
         let height = height
 
         let layersize = width * height
         for layerIndex in 0..<(data.count/layersize) {
-            layers.append([Int](data[layersize*layerIndex..<layersize*(layerIndex+1)]))
+            layers.append(try Layer(data: [Int](data[layersize*layerIndex..<layersize*(layerIndex+1)]),
+                                width: width, height: height))
         }
         var minZeroCount = Int.max
         for (index, layer) in layers.enumerated() {
-            var thisLayer = layer
+            var thisLayer = layer.getFlatArray()
             thisLayer.removeAll(where: { $0 != 0 })
             if thisLayer.count < minZeroCount {
                 minZeroCount = thisLayer.count
@@ -34,22 +39,62 @@ class DayEightSolver {
     }
 
     func solve() -> Int {
-        var onesLayer = layers[layerWithFewestZeroes]
+        var onesLayer = layers[layerWithFewestZeroes].getFlatArray()
         onesLayer.removeAll(where: { $0 != 1 })
         let onesCount = onesLayer.count
-        var twosLayer = layers[layerWithFewestZeroes]
+        var twosLayer = layers[layerWithFewestZeroes].getFlatArray()
         twosLayer.removeAll(where: { $0 != 2 })
         let twosCount = twosLayer.count
 
         return onesCount * twosCount
     }
+
+    func palimpsest() -> [[Int]] {
+        var result = layers[0].data
+        for (rowIndex, row) in result.enumerated() {
+            for (index, value) in row.enumerated() where value == 2 {
+                var colour = value
+                var layerIndex = 0
+                while colour == 2 {
+                    colour = layers[layerIndex].data[rowIndex][index]
+                    layerIndex += 1
+                }
+                result[rowIndex][index] = colour
+            }
+        }
+        return result
+    }
+}
+
+class Layer {
+    var data: [[Int]]
+    init(data inputdata: [Int], width: Int, height: Int) throws {
+        data = []
+        if inputdata.count/width != height {
+            throw LayerError.invalidDimensions
+        }
+        for row in 0..<(height) {
+            data.append([Int](inputdata[width*row..<width*(row+1)]))
+        }
+    }
+
+    func getFlatArray() -> [Int] {
+        var result = [Int]()
+        for row in data {
+            for value in row {
+                result.append(value)
+            }
+        }
+        return result
+    }
 }
 
 func dayeight(contents: String) throws -> String {
-    let partOne = DayEightSolver(data: contents, width: 25, height: 6)
-    let result = partOne.solve()
-//    let partTwo = Solver([5, 6, 7, 8, 9])
-//    _ = try partTwo.solvePartTwo(script: contents)
-    return "Part 1: \(result)"
-//Part 2: \(partTwo.getResult())"
+    let solver = try DayEightSolver(data: contents, width: 25, height: 6)
+    var result = "\n"
+    for row in solver.palimpsest() {
+        result.append("\(row)")
+        result.append("\n")
+    }
+    return "Part 1: \(solver.solve()) Part 2: \(result)"
 }
--- Day Eight/Tests/DayEightTests.swift
+++ Day Eight/Tests/DayEightTests.swift
@@ -6,6 +6,8 @@
 //  Copyright © 2020 Harold Dyson. All rights reserved.
 //
 
+// swiftlint:disable force_try
+
 import XCTest
 
 class DayEightTests: XCTestCase {
@@ -16,7 +18,7 @@ class DayEightTests: XCTestCase {
 
     func testFewestZeros() throws {
         let expected = 0
-        let solver = DayEightSolver(data: data, width: width, height: height)
+        let solver = try! DayEightSolver(data: data, width: width, height: height)
         let actual = solver.layerWithFewestZeroes
 
         XCTAssertEqual(expected, actual)
@@ -24,11 +26,18 @@ class DayEightTests: XCTestCase {
 
     func testSolvePartOne() throws {
         let expected = 1
-        let solver = DayEightSolver(data: data, width: width, height: height)
+        let solver = try! DayEightSolver(data: data, width: width, height: height)
         let actual = solver.solve()
 
         XCTAssertEqual(expected, actual)
-
     }
 
+    func testSolvePartTwo() {
+        let expected = [[0, 1], [1, 0]]
+
+        let solver = try! DayEightSolver(data: "0222112222120000", width: 2, height: 2)
+        let actual = solver.palimpsest()
+
+        XCTAssertEqual(expected, actual)
+    }
 }
