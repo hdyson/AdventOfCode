abb81ef584ed623441657472240d58ad9f7d6afc
Harold Dyson <hdyson@gmail.com>
2020-07-29 22:26:51 +0100

    Day 12 part 1

--- DayTwelve/DayTwelve.swift
+++ DayTwelve/DayTwelve.swift
@@ -18,7 +18,20 @@ struct DayTwelveSolver {
         // 3. For each moon, update velocities based on new moon positions
         // 4. Repeat for 1000 iterations
         // 5. Calculate energy in system
-        return 0
+        let moons = constructMoons(input: data)
+        for _ in 0..<1000 {
+            for moon in moons {
+                moon.applyGravity(moons: moons)
+            }
+            for moon in moons {
+                moon.applyVelocity()
+            }
+        }
+        var energy = 0
+        for moon in moons {
+            energy += moon.calculateEnergy()
+        }
+        return energy
     }
 
     func solvePartTwo() -> Int {
@@ -26,38 +39,90 @@ struct DayTwelveSolver {
     }
 }
 
-struct Position {
+struct Vector {
+    //swiftlint:disable identifier_name
     var x: Int
     var y: Int
     var z: Int
+    //swiftlint:enable identifier_name
+
+    static func - (lhs: Vector, rhs: Vector) -> Vector {
+        var result = Vector(x: 0, y: 0, z: 0)
+        if lhs.x > rhs.x {
+            result.x = -1
+        } else if lhs.x < rhs.x {
+            result.x = 1
+        }
+        if lhs.y > rhs.y {
+            result.y = -1
+        } else if lhs.y < rhs.y {
+            result.y = 1
+        }
+        if lhs.z > rhs.z {
+            result.z = -1
+        } else if lhs.z < rhs.z {
+            result.z = 1
+        }
+        return result
+    }
+
+    static func + (lhs: Vector, rhs: Vector) -> Vector {
+        var result = Vector(x: 0, y: 0, z: 0)
+        result.x = lhs.x + rhs.x
+        result.y = lhs.y + rhs.y
+        result.z = lhs.z + rhs.z
+        return result
+    }
 }
 
-struct Velocity {
-    var x: Int
-    var y: Int
-    var z: Int
-}
-
-struct Moon {
-    var position: Position
+class Moon {
+    var position: Vector
     // Initial velocity is always 0
-    var velocity = Velocity(x: 0, y: 0, z: 0)
+    var velocity = Vector(x: 0, y: 0, z: 0)
+
+    init(position positionParameter: Vector) {
+        position = positionParameter
+    }
+
+    // swiftlint:disable shorthand_operator
+    func applyGravity(moons: [Moon]) {
+        for moon in moons {
+            let difference = position - moon.position
+            velocity = velocity + difference
+        }
+    }
+
+    func applyVelocity() {
+        position = position + velocity
+    }
+    // swiftlint:enable shorthand_operator
+
+    func calculateEnergy() -> Int {
+        let potentialEnergy = abs(position.x) + abs(position.y) + abs(position.z)
+        let kineticEnergy = abs(velocity.x) + abs(velocity.y) + abs(velocity.z)
+        return potentialEnergy * kineticEnergy
+    }
 }
 
 func constructMoons(input: String) -> [Moon] {
     var moons = [Moon]()
     for line in input.components(separatedBy: "\n") {
         let components = line.components(separatedBy: ", ")
-        // Don't know width of number we're after, but do know widths of boilerplate surrounding values of
-        // interest.  So rather than pick out values, delete boilerplate.
-        var x = components[0]
-        x.removeFirst(3)
-        var y = components[1]
-        y.removeFirst(2)
-        var z = components[2]
-        z.removeFirst(2)
-        z.removeLast(1)
-        moons.append(Moon(position: Position(x: Int(x)!, y: Int(y)!, z: Int(z)!)))
+        // Ignore blank line at end of file:
+        if components.count > 1 {
+            // Don't know width of number we're after, but do know widths of boilerplate surrounding values of
+            // interest.  So rather than pick out values, delete boilerplate.
+            // swiftlint:disable identifier_name
+            var x = components[0]
+            x.removeFirst(3)
+            var y = components[1]
+            y.removeFirst(2)
+            var z = components[2]
+            // swiftlint:enable identifier_name
+            z.removeFirst(2)
+            z.removeLast(1)
+            moons.append(Moon(position: Vector(x: Int(x)!, y: Int(y)!, z: Int(z)!)))
+        }
     }
     return moons
 }
--- DayTwelve/Tests/DayTwelveTests.swift
+++ DayTwelve/Tests/DayTwelveTests.swift
@@ -8,7 +8,7 @@
 
 import XCTest
 
-class constructMoon: XCTestCase {
+class TestConstructMoon: XCTestCase {
 
     var moons=[Moon]()
 
